1f43e280da6079251221484baeb57e8f752bcdfc
wulei <xuxx5@asiainfo.com>
2018-11-28 14:30:52 +0800

    修改KafkaProducer+2

--- src/main/java/com/example/demo/controller/KafkaProducer.java
+++ src/main/java/com/example/demo/controller/KafkaProducer.java
@@ -17,7 +17,7 @@ public class KafkaProducer {
     private KafkaTemplate kafkaTemplate;
 
     public void send(String message){
-       logger.info("message++:",message);
+       logger.info("message:",message);
        kafkaTemplate.send("test",message);
     }
 }
